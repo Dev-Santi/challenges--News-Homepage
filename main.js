83a16a434778092d387b8e2e8180291b0ee86e99
santinakc <santinakcfin@gmail.com>
2023-01-27 23:43:40 -0300

    menu slider js

---
+++ main.js
@@ -0,0 +1,34 @@
+//navbar
+let body = document.getElementsByTagName('body')[0];
+
+let btnOpen = document.getElementById('open-btn');
+let btnClose = document.getElementById('close-btn');
+
+let opacity = document.getElementById('opacity-box');
+let menu = document.getElementById('menu');
+
+btnOpen.onclick = () => {
+    btnOpen.classList.remove('on');
+    btnOpen.classList.add('off');
+
+    btnClose.classList.remove('off')
+    btnClose.classList.add('on')
+
+    opacity.classList.toggle('on')
+    menu.classList.toggle('on')
+
+    body.classList.toggle('off')
+}
+
+btnClose.onclick = () => {
+    btnClose.classList.remove('on');
+    btnClose.classList.add('off');
+
+    btnOpen.classList.remove('off')
+    btnOpen.classList.add('on')
+
+    opacity.classList.toggle('on')
+    menu.classList.toggle('on')
+
+    body.classList.toggle('off')
+}
